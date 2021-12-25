5062eee1c85bdb9ceb9d7f1991686b63892cbe98
Ray Sandhu <aryamansandhu123@gmail.com>
2021-12-25 18:47:28 +0000

    Navbar CSS

--- my-lending-library/src/Navbar.js
+++ my-lending-library/src/Navbar.js
@@ -1,14 +1,17 @@
-import SearchBar from "./SearchBar"
 import { Link } from "react-router-dom"
+import { FaHome, FaBook } from "react-icons/fa";
 
 
 function Navbar() {
     return (
         <div className="navbar">
-            <div>
-                <h1 className="title">The Lending Library</h1> 
-                <Link to="/" style={{textAlign: "left"}} >Home Page</Link>
-                <Link to="/library" style={{textAlign: "right"}}>Library</Link>
+            <div> <Link to="/"  ><FaHome className= "home-icon" /></Link></div>
+            <div className = "title"> <h1 >The Lending Library</h1> </div>
+            <div style={{order: 3}} > 
+                <Link to="/library" >
+                    {//can add open book icon when on library page}
+                    <FaBook className= "library-icon"/>
+                </Link>
             </div>
         </div>
     )
