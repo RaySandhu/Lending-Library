7ec157756efac5ae967510a856f4b313ebf6e222
Ray Sandhu <aryamansandhu123@gmail.com>
2022-01-10 12:17:19 -0700

    Add New Modal added (non-functional)

--- my-lending-library/src/BookSearch.js
+++ my-lending-library/src/BookSearch.js
@@ -1,10 +1,11 @@
 function BookSearch() {
 
     function handleNewQuery(query) {
-        fetch(`https://www.googleapis.com/books/v1/volumes?q=intitle+"${query}"&key=${process.env.REACT_APP_SECRET_API_KEY}&maxResults=20`)
+        fetch(`https://www.googleapis.com/books/v1/volumes?q=intitle+"${query}"&key=${process.env.REACT_APP_SECRET_API_KEY}&maxResults=1`)
             .then(res => res.json())
-            .then(res => console.log(res.items.map(item => item.volumeInfo.title)))
+            .then(res => console.log(res.items.map(item => item.volumeInfo)))
             .catch(err => console.log(err))
+
     }
 
     return (
