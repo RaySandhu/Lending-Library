6358558ca521c76bbefbd7ca16a7e470d08610c8
Ray Sandhu <aryamansandhu123@gmail.com>
2022-02-15 16:41:00 -0700

    removing useEffects to prevent bug

--- my-lending-library/src/about-page/UserCorners.js
+++ my-lending-library/src/about-page/UserCorners.js
@@ -5,33 +5,31 @@ import ReadingStatus from "./ReadingStatus"
 function UserCorners() {
 
     const [userInfo, setUserInfo] = useState([])
+
+    let dbIndexes = ["RaysCornerIndex", "JoesCornerIndex"]
+    let cornerInfo = []
+    dbIndexes.forEach(async index => { 
+            const res = await getUserCornerResponse(index)
+            const response = await res[0]
+            cornerInfo.push(response)
+        })
     
-    useEffect(() => {
-        let dbIndexes = ["RaysCornerIndex", "JoesCornerIndex"]
-        let cornerInfo = []
-        dbIndexes.map(async index => { 
-                const res = await getUserCornerResponse(index)
-                cornerInfo.push(res[0])
-                return res
-            })
-            setUserInfo(cornerInfo)
-        }, [])
-    
-    console.log(userInfo[0])
-    const readingStatuses = userInfo.map(userResponse =>
-        <ReadingStatus
-            key = {userResponse.user}
-            user = {userResponse.user}
-            current = {userResponse.current}
-            next = {userResponse.next}
-            recommendations = {userResponse.recommendations}
-            tally = {userResponse.readingTally}
-        />
-    )
+ 
+    console.log(cornerInfo)
+    // const readingStatuses = userInfo.map(userResponse =>
+    //     <ReadingStatus
+    //         key = {userResponse.user}
+    //         user = {userResponse.user}
+    //         current = {userResponse.current}
+    //         next = {userResponse.next}
+    //         recommendations = {userResponse.recommendations}
+    //         tally = {userResponse.readingTally}
+    //     />
+    // )
 
     return (
         <div className="sidebar">
-            {readingStatuses}
+            {/* {readingStatuses} */}
         </div>
     )
 }
